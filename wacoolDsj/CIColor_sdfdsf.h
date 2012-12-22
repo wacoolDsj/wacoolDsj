8c818f4050a0a4fc573b783dc3571b26cf6a19c6
dengshengjin <dsj@localhost.(none)>
2012-12-22 17:27:15 +0800

    add

---
+++ wacoolDsj/CIColor_sdfdsf.h
@@ -0,0 +1,13 @@
+//
+//  CIColor_sdfdsf.h
+//  wacoolDsj
+//
+//  Created by dengshengjin on 12-12-22.
+//  Copyright (c) 2012年 dengshengjin. All rights reserved.
+//
+
+#import <CoreImage/CoreImage.h>
+
+@interface CIColor ()
+
+@end
